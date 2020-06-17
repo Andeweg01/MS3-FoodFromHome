061729316731ab4c379c2fb916a7d65af336da6a
Sascha Andeweg <sascha@designed.irish>
2020-06-17 17:28:04 +0000

    commit of wireframe pictures, some beautifying and additions to README.md

--- app.py
+++ app.py
@@ -149,6 +149,11 @@ def add_category():
     return render_template('addcategory.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0:8080'),
         port=int(os.environ.get('PORT', '5000')),
